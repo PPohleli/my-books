88eb2fc506c520feabe5a5675c1bdc27a4e9dded
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-06-14 15:13:47 +0200

    Add Publisher model and specify One-to-Many relationship with Book in EF.

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -8,5 +8,6 @@ namespace my_books.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options){}
 
         public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
