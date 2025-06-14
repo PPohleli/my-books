8612b41b4912a51ce3277c0964e3c690031643db
Phiwe_Pohleli <pohleliphiwe@gmail.com>
2025-06-14 17:07:16 +0200

    Add relational data with [HttpPost]

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -18,7 +18,7 @@ namespace my_books.Data.Services
             _context = context;
         }
 
-        public void AddBook(BookVM book)
+        public void AddBookWithAuthors(BookVM book)
         {
             var _book = new Book()
             {
@@ -27,12 +27,23 @@ namespace my_books.Data.Services
                 IsRead = book.IsRead,
                 DateRead = book.IsRead ? book.DateRead.Value : null,
                 Genre = book.Genre,
-                Author = book.Author,
                 CoverUrl = book.CoverUrl,
-                DateAdded = DateTime.Now
+                DateAdded = DateTime.Now,
+                PublisherId = book.PublisherId,
             };
             _context.Books.Add(_book);
             _context.SaveChanges();
+
+            foreach (var id in book.AuthorsIds)
+            {
+                var _book_author = new Book_Author()
+                {
+                    BookId = _book.Id,
+                    AuthorId = id
+                };
+                _context.Books_Authors.Add(_book_author);
+                _context.SaveChanges();
+            }
         }
 
         public List<Book> GetAllBooks() /*=> _context.Books.ToList();*/
@@ -59,7 +70,6 @@ namespace my_books.Data.Services
                 _book.DateRead = book.IsRead ? book.DateRead.Value : null;
                 _book.Rate = book.IsRead ? book.Rate.Value : null;
                 _book.Genre = book.Genre;
-                _book.Author = book.Author;
                 _book.CoverUrl = book.CoverUrl;
 
                 _context.SaveChanges();
--- my-books/Data/ViewModels/BookVM.cs
+++ my-books/Data/ViewModels/BookVM.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace my_books.Data.ViewModels
 {
@@ -10,7 +11,10 @@ namespace my_books.Data.ViewModels
         public DateTime? DateRead { get; set; }
         public int? Rate { get; set; }
         public string Genre { get; set; }
-        public string Author { get; set; }
         public string CoverUrl { get; set; }
+
+        public int PublisherId { get; set; }
+        public List<int> AuthorsIds { get; set; }
+
     }
 }
